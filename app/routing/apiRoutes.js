0dc496c92d9a172784c35c075ace97e950ab010d
Daniel A <dalamo90@gmail.com>
2018-05-24 09:05:17 -0500

    apiRoutes.js added empty array and linked friends.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,12 +6,10 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// These data sources hold arrays of information on friends.js.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-
+var friends = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
@@ -24,12 +22,8 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-  });
-
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
   });
 
   // API POST Requests
@@ -40,18 +34,18 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+    if (friends.length < 5) {
+      friends.push(req.body);
       res.json(true);
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+    // else {
+    //   waitListData.push(req.body);
+    //   res.json(false);
+    // }
   });
 
   // ---------------------------------------------------------------------------
@@ -60,9 +54,8 @@ module.exports = function(app) {
 
   app.post("/api/clear", function() {
     // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
+    friends = [];
 
-    console.log(tableData);
+    console.log(friends);
   });
 };
