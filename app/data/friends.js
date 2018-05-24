99ad6d2aff3b4e198883f0df90138073ba7de447
Daniel A <dalamo90@gmail.com>
2018-05-24 15:09:16 -0500

    friends.js added equation for possible matches

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,20 +2,42 @@
 var friends = [{
     "name":"Carrot Top",
     "photo":"/Users/ypham/Desktop/projects/FriendFinder/app/public/images/carrotTop600x470.jpg",
-    "scores":[
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
-      ]
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  }, {
+    "name":"George Foreman",
+    "photo":"/Users/ypham/Desktop/projects/FriendFinder/app/public/images/carrotTop600x470.jpg",
+    "scores": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  }, {
+    "name":"George Foreman",
+    "photo":"/Users/ypham/Desktop/projects/FriendFinder/app/public/images/carrotTop600x470.jpg",
+    "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  }, {
+    "name":"George Foreman",
+    "photo":"/Users/ypham/Desktop/projects/FriendFinder/app/public/images/carrotTop600x470.jpg",
+    "scores": [4, 4, 4, 4, 4, 4, 4, 4, 4, 4] 
+  }, {
+    "name":"George Foreman",
+    "photo":"/Users/ypham/Desktop/projects/FriendFinder/app/public/images/carrotTop600x470.jpg",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
   }];
   console.log(friends);
+
+  //after quiz
+  var userAnswers = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+
+  var bestFriend = "";
+  var difference = Infinity;
+// create own function and insert below
+  var answer = 0;
+  for (var i = 0; i < friends.length; i++) {//the potential friends
+    for (var j=0; j < userAnswers.length; j++) {
+        answer += userAnswers[j] - friends[i].scores[j];
+        if (answer < difference) {
+            difference = answer;
+            bestFriend = friends[i];
+        }
+    }
+  }
   // Note how we export the array. This makes it accessible to other files using require.
   module.exports = friends;
   
